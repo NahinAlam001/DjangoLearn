852cba31cac22df07aa43f4a3311c034230ae445
nap <hin@ns-MacBook-Air.local>
2022-10-28 23:09:30 +0600

    second django commit

---
+++ playground/templates/javascript/index.js
@@ -0,0 +1,10 @@
+function greet(name,lastname){
+    console.log('Hello '+name+' '+lastname);
+}
+greet('X', 'Y');
+greet('X', 'X');
+function square(number){
+    return number*number;
+}
+let number = square(2);
+console.log(number);
